760305001e163f50ab021be7eda9eb1f390eabc7
Ramii Ahmed <ramy@ahmedramy.com>
2021-07-04 12:53:33 +0000

    onyx: parts: Initialize High Touch Polling Rate support
    Used to handle our node to force enable Xiaomi's High Touch polling rate mode from kernel
    
    Adapted High Touch Polling Rate support to S style
    
    [byben] adapt for onyx
    
    Change-Id: I0fe57be0b75d529b46ac381e9a00fbbb3e4382af
    Signed-off-by: romgharti <romgharti@gmail.com>

--- parts/src/com/xiaomi/settings/BootCompletedReceiver.java
+++ parts/src/com/xiaomi/settings/BootCompletedReceiver.java
@@ -17,6 +17,7 @@ import android.view.Display;
 import android.view.Display.HdrCapabilities;
 
 import com.xiaomi.settings.display.ColorModeService;
+import com.xiaomi.settings.touchsampling.TouchSamplingUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final String TAG = "XiaomiParts";
@@ -39,6 +40,9 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         // Display
         context.startServiceAsUser(new Intent(context, ColorModeService.class),
                 UserHandle.CURRENT);
+
+        // High Touch polling rate
+        TouchSamplingUtils.restoreSamplingValue(context);
 
         // Override HDR types to enable Dolby Vision
         final DisplayManager displayManager = context.getSystemService(DisplayManager.class);
