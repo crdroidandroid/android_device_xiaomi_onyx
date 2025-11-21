b0b57e65d170d03d08786a5ac5e374aa5841490a
SGCMarkus <markusornik@gmail.com>
2025-11-21 11:09:22 +0000

    onyx: Set product characteristics to nosdcard Change-Id: Ief0e9d2ced9ebc7fc90300be697dd98c81ea5a48
    Signed-off-by: romgharti <romgharti@gmail.com>

--- device.mk
+++ device.mk
@@ -127,6 +127,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Characteristics
+PRODUCT_CHARACTERISTICS := nosdcard
+
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
