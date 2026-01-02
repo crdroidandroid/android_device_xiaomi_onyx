2269653e87e5ba494d417c30adb83613c403d0b8
romgharti <romgharti@gmail.com>
2026-01-02 12:59:03 +0000

    onyx: Add 12GB dalvik-heap prop * Some custom roms do not add 12288-dalvik-heap config on source. * onyx has only a 12GB ram variant.
    Signed-off-by: romgharti <romgharti@gmail.com>

--- device.mk
+++ device.mk
@@ -12,9 +12,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
-# Dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
-
 # pKVM
 $(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
 
